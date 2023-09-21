49535750fef46b25748d0c72808b943982085392
tadeogavensky <tadeogavensky.email@gmail.com>
2023-09-21 18:55:10 -0300

    module 4 and 5

--- M04_flujosdecontrol/Prep_Course_Homework_04.py
+++ M04_flujosdecontrol/Prep_Course_Homework_04.py
@@ -7,7 +7,13 @@
 
 # In[4]:
 
+num = -1
 
+if (num>0):
+    print(str(num) + " es mayor a 0")
+else:
+    print(str(num) + " es menor a 0")
+    
 
 
 
@@ -15,6 +21,14 @@
 
 # In[5]:
 
+a = 41
+b = "25"
+
+if(type(a)==type(int(b))):
+    print("Son del mismo tipo de dato: " + str(type(a)) )
+else:
+    print("No son del mismo tipo de dato")
+    
 
 
 
@@ -23,7 +37,16 @@
 
 # In[7]:
 
+a=20
 
+while (a>=1):
+    if(a%2==0):
+        print("El numero " +str(a)+ " es par")
+    else:
+        print("El numero " +str(a)+ " es impar")
+
+    a -=1
+        
 
 
 
@@ -31,7 +54,8 @@
 
 # In[9]:
 
-
+for i in range (0,6):
+    print("El valor de la potencia de " + str(i) + " al cubo es " +  str(i**3))
 
 
 
@@ -39,7 +63,10 @@
 
 # In[10]:
 
+num = 5
 
+for i in range(0,num):
+    print("Ciclo " + str(i))
 
 
 
@@ -47,7 +74,18 @@
 
 # In[33]:
 
-
+a = 12
+if (type(a) == int):
+    if (a > 0):
+        factorial = a
+        while (a > 2):
+            a = a - 1
+            factorial = factorial * a
+        print('El factorial es', factorial)
+    else:
+        print('La variable no es mayor a cero')
+else:
+    print('La variable no es un entero')
 
 
 
@@ -55,7 +93,14 @@
 
 # In[38]:
 
+a = 0
+while(a<20):
+    a += 1
+    for i in range (0,a):
+        print('Ciclo while nro ' + str(a))
+        print('Ciclo for nro ' + str(i))
 
+        
 
 
 
@@ -63,13 +108,32 @@
 
 # In[3]:
 
+a = 20
 
+for i in range (0,a):
+    while(a>0):
+        a-=1
+        print('Ciclo while nro ' + str(a))
+        print('Ciclo for nro ' + str(i))
 
 
 
 # 9) Imprimir los números primos existentes entre 0 y 30
 
 # In[54]:
+
+tope_rango=30
+n = 0
+primo = True
+while (n < tope_rango):
+    for div in range(2, n):
+        if (n % div == 0):
+            primo = False
+    if (primo):
+        print(n)
+    else:
+        primo = True
+    n += 1
 
 
 
@@ -79,7 +143,18 @@
 # In[55]:
 
 
-
+n = 0
+primo = True
+while (n < tope_rango):
+    for div in range(2, n):
+        if (n % div == 0):
+            primo = False
+            break
+    if (primo):
+        print(n)
+    else:
+        primo = True
+    n += 1
 
 
 # 11) En los puntos 9 y 10, se diseño un código que encuentra números primos y además se lo optimizó. ¿Es posible saber en qué medida se optimizó?
